3c26e9aae8c04902e2a3e31cde7450f50c00ee90
Mridhul <mridhul67@gmail.com>
2023-07-21 17:38:28 +0530

    Display confirmation password message upon invalid input Implement function to stop displaying the message after 3 seconds

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const form = document.querySelector(".form");
 const passField = document.querySelector(".password-field");
 const cpassField = document.querySelector(".cpassword-field");
+const cpassMessage = document.querySelector(".cpassword-message");
 
 function passCheck(event) {
   if (passField.value !== cpassField.value) {
     event.preventDefault();
+    cpassMessage.setAttribute("style", "opacity: 1;");
+    setTimeout(vanishMessage, 3000);
   }
+}
+
+function vanishMessage() {
+  cpassMessage.setAttribute("style", "opacity: 0;");
 }
 
 function colorValidation() {
